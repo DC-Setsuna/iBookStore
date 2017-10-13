17f7f238484246dcd25d2dadd911e49b7adf8c90
LawrenceDC <dengchenghh@yahoo.com>
2017-10-14 03:25:23 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+	<el-dialog title="iBook Store Login" :visible="this.$store.state.loginDialogVisible" :show-close="false">
+	  <el-tabs type="card" >
+	  	<el-tab-pane label="登录">
+	  		
+	  	</el-tab-pane>
+	  	<el-tab-pane label="注册">
+	  		
+	  	</el-tab-pane>
+	  </el-tabs>
+	  <div slot="footer" class="dialog-footer">
+	    <el-button @click="closeDialog">取 消</el-button>
+	    <el-button type="primary" @click="closeDialog">确 定</el-button>
+	  </div>
+	</el-dialog>
+</template>
+<script>
+export default {
+	data() {
+		return{
+			formLabelWidth: '120px',
+			form: {
+	          
+	        }
+		}
+	},
+	methods: {
+		closeDialog: function() {
+			this.$store.commit('loginDialog')
+		}
+	}
+}
+</script>
+<style>
+.el-tabs__nav {
+	width: 100%;
+}
+.el-tabs__item {
+	width: 50%;
+	text-align: center;
+}
+</style>
---
+++ src/components/Vheader.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="vheader">
+    <div class="header_con">
+      <div class="header_title">iBook Store</div>
+      <div class="header_nav">
+        <el-menu theme="dark" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="/"><i class="el-icon-menu"></i>首页</el-menu-item>
+          <el-menu-item index="/"><i class="el-icon-search"></i>高级搜索</el-menu-item>
+          <el-menu-item index="/">销售排行榜</el-menu-item>
+          <el-menu-item index="/">购物车<el-badge :value="car_count" /></el-menu-item>
+          <el-menu-item index="" v-if="!islogin">【请登录】</el-menu-item>
+          <el-submenu index="/" v-if="islogin">
+            <template slot="title">个人中心</template>
+            <el-menu-item index="/">我的信息</el-menu-item>
+            <el-menu-item index="/">订单详情</el-menu-item>
+            <el-menu-item index="/">退出登录</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
+    </div>
+    <Login/>
+  </div>
+</template>
+<script>
+import Login from './Login'
+	export default {
+		data() {
+			return {
+				car_count: 2,
+				islogin: false,
+			}
+		},
+		methods: {
+			handleSelect(key, keypath) {
+				console.log(key,keypath)
+				if (key == '') {
+					this.$store.commit('loginDialog')
+				}
+			}
+		},
+		components: { Login }
+	}
+</script>
+<style>
+.vheader {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  background-color: #334258;
+  height: 60px;
+}
+
+.header_title {
+  height: 60px;
+  line-height: 60px;
+  color: #37e0ec;
+  font-size: 22px;
+  cursor: default;
+  float: left;
+  font-weight: 700px;
+}
+
+.header_con {
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+.header_nav {
+  float: right;
+}
+</style>
